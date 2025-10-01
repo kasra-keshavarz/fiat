588619f87241bb6ab69eaa98df27f52d1e0e1270
Kasra Farmer <kasra.keshavarz1@ucalgary.ca>
2025-10-01 16:23:48 -0600

    Backing up

---
+++ src/fiatmodel/calibration/ostrich/default_dicts.py
@@ -0,0 +1,35 @@
+'''
+Limited dictionaries with names consistent among other "calibration"
+methods.
+'''
+
+_algorithm_equivalence = {
+    'bisectionalgorithm': 'BisectionAlg',
+    'fletcher-reeves': 'FletchReevesAlg',
+    'levenberg-marquardt': 'LevMar',
+    'gml-ms': 'LevMar',
+    'gridalgorithm': 'GridAlg',
+    'powell': 'PowellAlg',
+    'steepest-descent': 'SteepestDescAlg',
+    'appso': 'APPSO',
+    'particleswarm': 'ParticleSwarm',
+    'beers': 'BEERS',
+    'binarygeneticalgorithm': 'GeneticAlg',
+    'geneticalgorithm': 'GeneticAlg',
+    'discretesimulatedannealing': 'SimulatedAlg',
+    'simulatedannealing': 'SimulatedAlg',
+    'vanderbiltsimulatedannealing': 'SimulatedAlg',
+    'discretedds': 'DiscreteDDSAlg',
+    'dds': 'DDSAlg',
+    'paralleldds': 'ParallelDDSAlg',
+    'shuffledcomplexevolution': 'SCEUA',
+    'samplingalgorithm': 'SamplingAlg',
+    'ddsau': '_DDSAU_Alg',
+    'glue': 'GLUE',
+    'metropolissampler': 'MetropolisSampler',
+    'rejectionsampler': 'RejectionSampler',
+    'padds': 'PADDSAlg',
+    'parapadds': 'ParallelPADDSAlg',
+    'smooth': 'SMOOTH',
+    '': '',
+}
